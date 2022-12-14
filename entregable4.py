54ab85eb527d8ea7abe72d3a619149f37c92575f
al404832 <al404832@uji.es>
2022-12-14 01:13:37 +0100

    copia de seguridad

--- entregable4.py
+++ entregable4.py
@@ -14,8 +14,14 @@ def read_data(f: TextIO) -> list[int]:
 
 def process(v: list[int]) -> Solution:
     picos = calcula_picos(v)
-    sol = (0,1,0)
-    return calcula_valle(picos, v)
+    picos = limpia_picos (picos, v)
+    solucion = (0, 1, 0)
+    for i in range (1, len(picos), 1):
+        aux = calcula_valle(i-1, i, v)
+        if aux[2] > solucion[2]:
+            solucion = aux
+
+    return solucion
 
 
 def calcula_picos(v: list[int]) -> list[int]:
@@ -38,8 +44,40 @@ def calcula_picos(v: list[int]) -> list[int]:
     return picos
 
 
-def calcula_valle(auxI: int, auxD: int, v: list[int]) -> Solution:
-    return None
+def limpia_picos(picos: list[int], v: list[int]) -> list[int]:
+    picos_limpios = [picos[0]]
+    aux = 0
+    for i in range(1, len(picos), 1):
+        if v[picos[i]] >= v[picos[aux]]:
+            picos_limpios.append(picos[i])
+            aux = i
+
+
+    return picos_limpios
+
+
+def calcula_valle(l: int, r: int, v: list[int]) -> Solution:
+    l_mayor = True
+    tam = 0
+    min = r
+
+    if v[r] > v[l]:
+        l_mayor = False
+        min = l
+
+    if l_mayor:
+        while v[l] > v[r]:
+            l += 1
+        l -= 1
+    else:
+        while v[r] > v[l]:
+            r -= 1
+        r += 1
+
+    for i in range(l + 1, r, 1):
+        tam += v[min] - v[i]
+
+    return l, r, tam
 
 
 def show_results(sol: Solution):
